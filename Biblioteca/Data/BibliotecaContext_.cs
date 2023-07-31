b73f3e354b0c6cf39f4844d226ea18530a7f78d6
carlos.ebcosta2 <carlos.ebcosta2@ACL0533575W10-1>
2023-07-31 16:29:46 -0300

    Inserindo os dados de conexão

---
+++ Biblioteca/Data/BibliotecaContext_.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
+{
+	public class BibliotecaContext_: DbContext
+	{
+		public BibliotecaContext_(DbContextOptions<BibliotecaContext_> options) : base(options)
+		{ 
+		
+		}
+
+	}
+}
